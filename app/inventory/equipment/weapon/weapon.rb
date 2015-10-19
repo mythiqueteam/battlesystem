e20fd6f8b816497e11a37ff252a1be1700d45320
ldocao <ldocao@gmail.com>
2015-10-19 15:54:58 +0200

    fix: call modules

--- app/inventory/equipment/weapon/weapon.rb
+++ app/inventory/equipment/weapon/weapon.rb
@@ -1,10 +1,19 @@
-require_relative "../../stats/status.rb"
+require_relative "../../../stats/status.rb"
+require_relative "../../object.rb"
 
-module Weapon
-    DEFAULT_WEAPON = nil
-    class Weapon < Equipment::Object
-        def initialize(bonus=Stats::DEFAULT_STATUS)
-            @bonus = bonus
+module Inventory
+    module Equipment
+        class Weapon < Inventory::Object
+            attr_reader :bonus, :category
+            def initialize(bonus=Stats::DEFAULT_STATUS)
+                @bonus = bonus
+                @category = "Weapon"
+            end
         end
     end
 end
+
+
+
+sword = Inventory::Equipment::Weapon.new()
+puts sword.bonus, sword.category
