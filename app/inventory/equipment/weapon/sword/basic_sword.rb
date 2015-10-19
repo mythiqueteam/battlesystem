32425371b8bd15afaeeac42a0b785f61b632005d
ldocao <ldocao@gmail.com>
2015-10-19 16:23:30 +0200

    doc: setup bonus

--- app/inventory/equipment/weapon/sword/basic_sword.rb
+++ app/inventory/equipment/weapon/sword/basic_sword.rb
@@ -12,6 +12,8 @@ module Inventory
             def initialize()
                 @name = "Basic Sword"
                 @weight = Inventory::DEFAULT_WEIGHT
+
+                #setup bonus
                 hit_point = Stats::HitPoint.new(10,10)
                 magic_point = Stats::MagicPoint.new(0,0)
                 force = Stats::Force.new(1)
