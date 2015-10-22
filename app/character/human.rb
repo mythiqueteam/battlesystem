82fdb3614a877fe8aa79b70bb15330f8c8312c06
ldocao <ldocao@gmail.com>
2015-10-22 09:31:53 +0200

    feat(character): birthday attribute and age method

--- app/character/human.rb
+++ app/character/human.rb
@@ -1,11 +1,19 @@
+require_relative "./character.rb"
+
 module Character
     class Human
-        attr_reader :first_name, :last_name
+        attr_reader :first_name, :last_name, :birthday
         
-        def initialize(first_name, last_name)
+        def initialize(first_name, last_name, birthday=Common::UNKNOWN)
             @first_name = first_name
             @last_name = last_name
+            @birthday = birthday
+        end
+
+        def age(time_ingame)
+            time_ingame - birthday
         end
     end
 end
+
 
