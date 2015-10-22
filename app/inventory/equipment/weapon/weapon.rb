89551db905bdc8ba55f17369dd7e316dadfe813d
ldocao <ldocao@gmail.com>
2015-10-22 10:47:46 +0200

    refactor(equipment): separate piece of equipment from set of equipment

--- app/inventory/equipment/weapon/weapon.rb
+++ app/inventory/equipment/weapon/weapon.rb
@@ -1,7 +1,7 @@
 require_relative "../equipment.rb"
 
 module Inventory
-    class Weapon < Inventory::Equipment
+    class Weapon < Inventory::PieceOfEquipment
         attr_reader :bonus, :category
         def initialize(bonus=Stats::DEFAULT_STATUS)
             @bonus = bonus
