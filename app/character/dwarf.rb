50f0957bb7be96ae22b77218ed49f19f6ec04b11
ldocao <ldocao@gmail.com>
2015-10-22 10:07:25 +0200

    feat(character): create dwarf class as example for another hero race

---
+++ app/character/dwarf.rb
@@ -0,0 +1,19 @@
+require_relative "./character.rb"
+
+
+# dwarf don't have first and last names, but a name only. Dwarf's father's name must be known. currently set to unknown
+module Character
+    class Dwarf < Character::Living
+        attr_reader :name, :father, :birthday
+        
+        def initialize(name, father, 
+                       birthday=Common::UNKNOWN)
+            @name = name
+            @father = Common::UNKNOWN #FIXME
+            @birthday = birthday
+        end
+
+    end
+end
+
+
