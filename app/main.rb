f192b35e2aed9b9f86efb77550acba7aefabc631
ldocao <ldocao@gmail.com>
2015-10-22 11:08:59 +0200

    noise: test for character saving changes before rebase inventory

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,6 @@
 require_relative "inventory/equipment/weapon/sword/basic_sword.rb"
 require_relative "inventory/equipment/armor/plate/basic_plate.rb"
 
-
 sword = Inventory::BasicSword.new()
 plate = Inventory::BasicPlate.new()
 equipment = Inventory::Equipment.new(weapon=sword, plate=plate)
