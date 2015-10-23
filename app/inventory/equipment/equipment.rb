2ab26dbb73d17276565d05d9ea5279d1aa83e35c
ldocao <ldocao@gmail.com>
2015-10-23 13:26:42 +0200

    doc(equipment): sum_bonus

--- app/inventory/equipment/equipment.rb
+++ app/inventory/equipment/equipment.rb
@@ -12,36 +12,19 @@ module Inventory
         end
 
 
-        # Sum the bonus for all pieces in equipment
+        # Sum the bonus of all pieces of equipment for a given attribute (eg: HP, Force)
         # 
-        # @equipment[Equipment] Set of weapon, armor, accessories
-        #
-        # @return [Status] Sum of all bonus
-        def sum_bonus
-            ## initialisation
-            total_hp = Stats::HitPoint.new(0,Stats::MAX_HP)
-            total_mp = Stats::MagicPoint.new(0,Stats::MAX_MP)
-            total_force = Stats::DEFAULT_FORCE
-            total_defense = Stats::DEFAULT_DEFENSE
-            total_dexterity = Stats::DEFAULT_DEXTERITY
-            total_luck = Stats::DEFAULT_LUCK
-
-            ##sum bonus for each piece of equipment
+        # @attribute [String] Attribute name to sum over (eg: "@hit_point", "@force")
+        # 
+        # @return [value] Sum of bonus for the given attribute given by equipment 
+        def sum_bonus(attribute)
             all_pieces = self.instance_variables
+            total = 0 #initialize output
             for piece in all_pieces do
-                total_hp += self.instance_variable_get(piece).bonus.hit_point
-                total_mp += self.instance_variable_get(piece).bonus.magic_point
-                total_force += self.instance_variable_get(piece).bonus.force
-                total_defense += self.instance_variable_get(piece).bonus.defense
-                total_dexterity += self.instance_variable_get(piece).bonus.dexterity
-                total_luck += self.instance_variable_get(piece).bonus.luck
+                stat = self.instance_variable_get(piece).bonus
+                total += stat.instance_variable_get(attribute)
             end
-            Stats::Status.new(hit_point: total_hp,
-                              magic_point: total_mp,
-                              force: total_force,
-                              defense: total_defense,
-                              dexterity: total_dexterity,
-                              luck:total_luck)
+            total
         end
         
         def to_s
