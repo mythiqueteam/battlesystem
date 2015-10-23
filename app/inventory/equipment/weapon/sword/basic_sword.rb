70abe0235830c559db4009877662958c923866d9
ldocao <ldocao@gmail.com>
2015-10-23 09:43:16 +0200

    fix(equipment): setup only bonus stats as they are optional keywords now

--- app/inventory/equipment/weapon/sword/basic_sword.rb
+++ app/inventory/equipment/weapon/sword/basic_sword.rb
@@ -9,10 +9,9 @@ module Inventory
 
             #setup bonus
             hit_point = Stats::HitPoint.new(10,10)
-            magic_point = Stats::MagicPoint.new(0,0)
             force = Stats::Force.new(1)
             defense = Stats::Defense.new(1)
-            @bonus = Stats::Status.new(hit_point, magic_point, force, defense)
+            @bonus = Stats::Status.new(hit_point=hit_point, force=force, defense=defense)
         end
     end
 end
