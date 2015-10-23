39011772e1818ae04c258fad0f0d0b12145929ac
ldocao <ldocao@gmail.com>
2015-10-23 09:40:21 +0200

    feat(character): add dexterity and luck stats

---
+++ app/stats/dexterity.rb
@@ -0,0 +1,16 @@
+require_relative "../common/unit.rb"
+require_relative "./stats.rb"
+
+module Stats
+
+    class Dexterity < Common::Value
+        def initialize(value)
+            @value = value
+            @min = Stats::MIN_STAT_VALUE
+            @max = Stats::MAX_STAT_VALUE
+            @unit = Common::DEXTERITY_UNIT
+        end
+    end
+
+    DEFAULT_DEXTERITY = Dexterity.new(0)
+end
