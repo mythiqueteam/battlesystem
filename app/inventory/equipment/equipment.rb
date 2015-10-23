cc26330e546034be3242e52e4984a08aa7d570e5
ldocao <ldocao@gmail.com>
2015-10-23 09:32:23 +0200

    feat(equipment): summary bonus

--- app/inventory/equipment/equipment.rb
+++ app/inventory/equipment/equipment.rb
@@ -3,6 +3,33 @@ require_relative "weapon/weapon.rb"
 require_relative "armor/plate/plate.rb"
 
 module Inventory
+
+
+    # Sum the bonus for all pieces in equipment
+    # 
+    # @equipment[Equipment] Set of weapon, armor, accessories
+    #
+    # @return [Status] Sum of all bonus
+    def self.summary_bonus(equipment)
+        ## initialisation
+        total_hp = Stats::HitPoint.new(0,Stats::MAX_HP)
+        total_mp = Stats::MagicPoint.new(0,Stats::MAX_MP)
+        total_force = Stats::DEFAULT_FORCE
+        total_defense = Stats::DEFAULT_DEFENSE
+
+        ##sum bonus for each piece of equipment
+        all_pieces = equipment.instance_variables
+        for piece in all_pieces do
+            total_hp += equipment.instance_variable_get(piece).bonus.hit_point
+            total_mp += equipment.instance_variable_get(piece).bonus.magic_point
+            total_force += equipment.instance_variable_get(piece).bonus.force
+            total_defense += equipment.instance_variable_get(piece).bonus.defense
+        end
+        Stats::Status.new(total_hp, total_mp, total_force, total_defense)
+    end
+
+
+
     class Equipment
         def initialize(weapon, plate)
             @weapon = weapon
@@ -15,6 +42,12 @@ module Inventory
             "("+list_bonus+")"
         end
     end
+    
     DEFAULT_EQUIPMENT = Inventory::Equipment.new(Inventory::DEFAULT_WEAPON,
                                                  Inventory::DEFAULT_PLATE)
 end
+
+
+equipment = Inventory::DEFAULT_EQUIPMENT
+total_bonus = Inventory::summary_bonus(equipment)
+puts total_bonus
