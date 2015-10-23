491c67914d1e155969c4b32874e8e2f8bc66380c
ldocao <ldocao@gmail.com>
2015-10-23 11:10:39 +0200

    fix(character): is_dead attribute is initialized

--- app/character/monster/succube.rb
+++ app/character/monster/succube.rb
@@ -4,13 +4,16 @@ module Character
     module Monster 
         class Succube < Battle::Fighter
             attr_reader :name, :level, :status
+            attr_accessor :is_dead
             def initialize(level=Stats::DEFAULT_LEVEL, 
                            status=Stats::DEFAULT_STATUS,
-                           equipment=Inventory::DEFAULT_EQUIPMENT)
+                           equipment=Inventory::DEFAULT_EQUIPMENT,
+                           is_dead=false)
                 @name = "Succube"
                 @level = level
                 @status = status
                 @equipment = equipment
+                @is_dead = is_dead
             end
         end
     end
