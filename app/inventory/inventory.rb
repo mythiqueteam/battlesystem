abdd08ea9bbefc24332bed396a9c77ec40bf5610
ldocao <ldocao@gmail.com>
2015-10-23 10:47:50 +0200

    fix(equipment): require relative

--- app/inventory/inventory.rb
+++ app/inventory/inventory.rb
@@ -5,6 +5,8 @@ require_relative "../stats/hit_point.rb"
 require_relative "../stats/magic_point.rb"
 require_relative "../stats/force.rb"
 require_relative "../stats/defense.rb"
+require_relative "../stats/dexterity.rb"
+require_relative "../stats/luck.rb"
 
 module Inventory
 
