35feb997c1a0781967c7e9051f601f04b3d39c21
ldocao <ldocao@gmail.com>
2015-10-23 09:33:06 +0200

    feat(equipment): add name attribute

--- app/inventory/equipment/armor/plate/plate.rb
+++ app/inventory/equipment/armor/plate/plate.rb
@@ -3,7 +3,8 @@ require_relative "../armor.rb"
 module Inventory
     class Plate < Inventory::Armor
         attr_reader :category
-        def initialize()
+        def initialize(name="None")
+            @name = name
             @bonus = Stats::DEFAULT_STATUS
             @category = ["Armor", "Plate"]
         end
