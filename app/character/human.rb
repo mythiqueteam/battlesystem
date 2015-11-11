279a09204e195fb68e5e638a792422bc79b9d05e
Long Do Cao <ldocao@gmail.com>
2015-11-11 19:31:56 +0100

    refactor: to_s method is more idiomatic

--- app/character/human.rb
+++ app/character/human.rb
@@ -1,19 +1,19 @@
 require_relative "./character.rb"
 
 module Character
-    class Human < Character::Living
-        attr_reader :first_name, :last_name, :birthday
-        
-        def initialize(first_name, last_name, birthday=Common::UNKNOWN)
-            @first_name = first_name
-            @last_name = last_name
-            @birthday = birthday
-        end
-
-        def to_s
-            @first_name+" "+@last_name+" : "+@level.to_s
-        end
+  class Human < Character::Living
+    attr_reader :first_name, :last_name, :birthday
+    
+    def initialize(first_name, last_name, birthday=Common::UNKNOWN)
+      @first_name = first_name
+      @last_name = last_name
+      @birthday = birthday
     end
+
+    def to_s
+      "#{@first_name} #{@last_name} #{@level.to_s}"
+    end
+  end
 end
 
 
